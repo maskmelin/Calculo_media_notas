678c2f00027b0e48ac91d7de5ccb2314464b5b55
Rafael <rafaelarouche@hotmail.com>
2022-12-14 16:43:49 -0300

    "calculo medias"

---
+++ medias.py
@@ -0,0 +1,17 @@
+#projeto média aritmética
+
+Nota1 = input("Digite a primeira nota:")
+temp1 = int(Nota1)
+
+Nota2 = input ("Digite a segunda nota:")
+temp2 = int(Nota2)
+
+Nota3 = input ("Digite a terceira nota:")
+temp3 = int(Nota3)
+
+Nota4 = input ("Digite a quarta nota:")
+temp4 = int(Nota4)
+
+media = ((temp1+temp2+temp3+temp4)/4)
+
+print ("A média aritmética é", media)
